3eb8cf9b2e49a1dfd984d550617dccc52cc7ec1c
noor-ul-amin0 <noorulamindev0@gmail.com>
2024-07-30 09:57:06 +0500

    chore: Delete unnecessary scripts

--- utils.py
+++
@@ -1,8 +0,0 @@
-import uuid # universally unique identifier
-from datetime import datetime
-
-def generate_id():
-    return str(uuid.uuid4())
-
-def get_current_date():
-    return datetime.utcnow().isoformat()
