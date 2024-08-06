ad3488adf80367bec6a6148dbdfedd8bcbb9c963
noor-ul-amin0 <noorulamindev0@gmail.com>
2024-08-06 20:23:29 +0500

    chore: Refactor routes

--- routers/task_router.py
+++ routers/task_router.py
@@ -6,9 +6,11 @@ router = APIRouter()
 
 task_manager = TaskManager()
 
+
 @router.get("/tasks")
 def read_tasks():
     return {"success": True, "data": task_manager.get_tasks()}
+
 
 @router.get('/tasks/{task_id}')
 def read_task(task_id: str):
@@ -17,10 +19,12 @@ def read_task(task_id: str):
         raise HTTPException(status_code=404, detail="Task not found")
     return {"success": True, "data": task}
 
+
 @router.post('/tasks')
 def create_task(task: Task):
     new_task = task_manager.add_task(task)
     return {"success": True, "data": new_task}
+
 
 @router.put('/tasks/{task_id}')
 def update_task(task_id: str, task: Task):
@@ -28,6 +32,7 @@ def update_task(task_id: str, task: Task):
     if updated_task is None:
         raise HTTPException(status_code=404, detail="Task not found")
     return {"success": True, "data": updated_task}
+
 
 @router.delete('/tasks/{task_id}')
 def delete_task(task_id: str):
